e79af2bcfd4364c74c648375fc2c78fdac94b3fe
machado-jose <machado.josegustavo@gmail.com>
2020-04-29 16:57:50 -0300

    Get form datas

--- src/controller/WhatsappCloneController.js
+++ src/controller/WhatsappCloneController.js
@@ -83,6 +83,22 @@ class WhatsappCloneController
 		{
 			return this.classList.contains(name);
 		}
+
+		HTMLFormElement.prototype.getForm = function()
+		{
+			return new FormData(this);
+		}
+
+		HTMLFormElement.prototype.toJSON = function()
+		{
+			let json = {};
+			this.getForm().forEach((value, key)=>{
+				json[key] = value;
+			})
+			return json;
+		}
+
+
 	}
 
 	initEvents()
@@ -109,6 +125,26 @@ class WhatsappCloneController
 
 		this.el.btnClosePanelAddContact.on('click', ()=>{
 			this.el.panelAddContact.removeClass('open');
+		});
+
+		this.el.photoContainerEditProfile.on('click', ()=>{
+			this.el.inputProfilePhoto.click();
+		});
+
+		this.el.inputNamePanelEditProfile.on('keypress', (e)=>{
+			if(e.key === 'Enter')
+			{
+				e.preventDefault();
+				this.el.btnSavePanelEditProfile.click();
+			}
+		});
+
+		this.el.btnSavePanelEditProfile.on('click', (e)=>{
+			console.log(this.el.inputNamePanelEditProfile.innerHTML);
+		});
+
+		this.el.formPanelAddContact.on('submit', (e)=>{
+			e.preventDefault();
 		});
 	}
 
