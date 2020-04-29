bd396494938f8c6849784eddba40b05d64ab60c8
machado-jose <machado.josegustavo@gmail.com>
2020-04-29 15:38:45 -0300

    Manipulating Prototype

--- src/controller/WhatsappCloneController.js
+++ src/controller/WhatsappCloneController.js
@@ -2,9 +2,14 @@ class WhatsappCloneController
 {
 	constructor()
 	{
+		//Esse método vai permitir que vários elementos diferentes possam herdar
+		//métodos sem necessidade de especificar cada um separadamente. Semelhante 
+		//do JQuery
+		this.elementsPrototype();
 		//Carregar automaticamente os elementos html. Para isso, os nomes dos id devem
 		//seguir o padrao nome-nome
 		this.loadElements();
+
 	}
 
 	loadElements()
@@ -14,5 +19,67 @@ class WhatsappCloneController
 		document.querySelectorAll("[id]").forEach(element=>{
 			this.el[Format.getCamelCase(element.id)] = element;
 		});
+	}
+
+	elementsPrototype()
+	{
+		Element.prototype.hide = function()
+		{
+			this.style.display = "none";
+			return this;
+		}
+
+		Element.prototype.show = function()
+		{
+			this.style.display = "block";
+			return this;
+		}
+
+		Element.prototype.toggle = function()
+		{
+			this.style.display = (this.style.display === 'block') ? "none" : "block";
+			return this;
+		}
+
+		Element.prototype.on = function(events, fn)
+		{
+			events.split(' ').forEach(event=>{
+				this.addEventListener(event, fn);
+			});
+			return this;
+		}
+
+		Element.prototype.css = function(styles)
+		{
+			for(let name in styles)
+			{
+				console.log(name);
+				this.style[name] = styles[name];
+			}
+			return this;
+		}
+
+		Element.prototype.addClass = function(name)
+		{
+			this.classList.add(name);
+			return this;
+		}
+
+		Element.prototype.removeClass = function(name)
+		{
+			this.classList.remove(name);
+			return this;
+		}
+
+		Element.prototype.toggleClass = function(name)
+		{
+			this.classList.toggle(name);
+			return this;
+		}
+
+		Element.prototype.hasClass = function(name)
+		{
+			return this.classList.contains(name);
+		}
 	}
 }
