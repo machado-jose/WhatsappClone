eeaa5effcfd7cf4f0e9332bb42c63105b163dec2
machado-jose <machado.josegustavo@gmail.com>
2020-04-30 15:06:16 -0300

    Format the record time

--- src/controller/WhatsappCloneController.js
+++ src/controller/WhatsappCloneController.js
@@ -265,7 +265,7 @@ class WhatsappCloneController
 	{
 		let start = Date.now();
 		this._recordMicrophoneInterval = setInterval(()=>{
-			this.el.recordMicrophoneTimer.innerHTML = (Date.now() - start);
+			this.el.recordMicrophoneTimer.innerHTML = (Format.toTime(Date.now() - start));
 		}, 100);
 	}
 
--- src/utils/Format.js
+++ src/utils/Format.js
@@ -8,4 +8,13 @@ class Format
 		div.innerHTML = `<div data-${text}="id"></div>`;
 		return Object.keys(div.firstChild.dataset)[0];
 	}
+
+	static toTime(duration)
+	{
+		let seconds = parseInt((duration / 1000) % 60);
+		let minutes = parseInt((duration / (1000 * 60)) % 60);
+		let hours = parseInt((duration / (1000 * 60 * 60)) % 24);
+
+		return (hours > 0) ? `${hours}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}` : `${minutes}:${seconds.toString().padStart(2, '0')}`;
+	}
 }
