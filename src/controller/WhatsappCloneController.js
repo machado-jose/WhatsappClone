cb7b725e1c5e76f56b3680e5c813312383e7d952
machado-jose <machado.josegustavo@gmail.com>
2020-05-08 18:21:47 -0300

    Config Scroll Messages

--- src/controller/WhatsappCloneController.js
+++ src/controller/WhatsappCloneController.js
@@ -192,9 +192,14 @@ export class WhatsappCloneController
     		display: 'flex'
     	});
 
+    	this.el.panelMessagesContainer.innerHTML = '';
+
     	Message.getRef(this._contactActive.chatId).orderBy('timeStamp').onSnapshot(docs=>{
-    		
-    		this.el.panelMessagesContainer.innerHTML = '';
+
+    		let scrollTop = this.el.panelMessagesContainer.scrollTop;
+			let scrollTopMax = (this.el.panelMessagesContainer.scrollHeight -
+				this.el.panelMessagesContainer.offsetHeight);
+			let autoScroll = (scrollTop >= scrollTopMax);
 
     		docs.forEach(doc=>{
     			let data = doc.data();
@@ -202,15 +207,28 @@ export class WhatsappCloneController
 
     			if(!this.el.panelMessagesContainer.querySelector('#_' + data.id))
     			{
+
     				let message = new Message();
 	    			message.fromJSON(data);
 
 	    			let me = (data.from === this._user.email);
 	    			let view = message.getViewElement(me);
 	    			this.el.panelMessagesContainer.appendChild(view);
+
     			}
     
     		});
+
+    		if(autoScroll)
+			{
+				this.el.panelMessagesContainer.scrollTop = 
+					(this.el.panelMessagesContainer.scrollHeight -
+					this.el.panelMessagesContainer.offsetHeight);
+			}
+			else
+			{
+				this.el.panelMessagesContainer.scrollTop = scrollTop;
+			}
     	});
 	}
 
